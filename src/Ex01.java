65fe3e7f5606cca29a8aa814b77f4d2f1f1c10db
ricardobarp <ricardobarp@yahoo.com>
2026-03-03 21:42:22 -0300

    Resolução do Exercício 01
    Exercício 01:
    
    Objetivo: utilizar o método public String concat(String str)
    para concatenar nome e sobrenome em uma variável String.

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,7 +1,19 @@
 import utilitarios.*;
 
 public class Ex01 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String nome;
+        String sobrenome;
+        String nomeCompleto;
+
+        nome = Teclado.solicitarString
+        ("Informe seu nome: ");
+
+        sobrenome = Teclado.solicitarString
+        ("Informe seu sobrenome: ");
+
+        nomeCompleto = nome.concat(" " + sobrenome);
+        Video.exibirMensagem("O seu nome completo é: " + nomeCompleto);
     }
+
 }
